dd575f691644a7e3933873616340b42d092914d1
Thomas Maurice <thomas@maurice.fr>
2021-06-28 12:17:46 +0100

    Updates to install prometheus

--- cluster-up.sh
+++ cluster-up.sh
@@ -15,6 +15,7 @@ sleep 10
 
 # Install stuff via Helm
 helm repo add kubernetes-dashboard https://kubernetes.github.io/dashboard/
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo update
 helm install kubernetes-dashboard kubernetes-dashboard/kubernetes-dashboard \
     --namespace kubernetes-dashboard \
@@ -22,3 +23,9 @@ helm install kubernetes-dashboard kubernetes-dashboard/kubernetes-dashboard \
     --set serviceAccount.create=false \
     --set serviceAccount.name=admin-user \
     --set metricsScraper.enabled=true
+helm install prometheus -n monitoring prometheus-community/kube-prometheus-stack
+
+sleep 5
+echo "http://grafana.localhost crededntials: "
+kubectl get secret -n monitoring prometheus-grafana -oyaml | grep admin-password| cut -d: -f2|tr -d \  | base64 -d
+kubectl get secret -n monitoring prometheus-grafana -oyaml | grep admin-user| cut -d: -f2|tr -d \  | base64 -d
